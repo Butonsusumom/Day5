12a5f236d9caa4d9f0413e28cfdeedf4399839a7
Ksusha <butonsusumom@gmail.com>
2020-12-09 13:56:06 +0300

    Initial commit

---
+++ src/main/java/com/tsybulko/day5/exception/TextProcessingException.java
@@ -0,0 +1,18 @@
+package com.tsybulko.day5.exception;
+
+public class TextProcessingException extends Exception {
+    public TextProcessingException() {
+    }
+
+    public TextProcessingException(String message) {
+        super(message);
+    }
+
+    public TextProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TextProcessingException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/com/tsybulko/day5/file/FileTextReader.java
@@ -0,0 +1,21 @@
+package com.tsybulko.day5.file;
+
+import com.tsybulko.day5.exception.TextProcessingException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileTextReader {
+    public String read(String fileName) throws TextProcessingException {
+        try {
+            return new String(Files.readAllBytes(Paths.get(fileName)));
+        } catch (IOException e) {
+            throw new TextProcessingException("Error during reading file!",
+                    e.getCause());
+        } catch (NullPointerException e) {
+            throw new TextProcessingException("Null pointer detected!",
+                    e.getCause());
+        }
+    }
+}
